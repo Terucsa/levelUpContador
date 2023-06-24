ac33c993e0e0c3fb1214c3546eed4d1b6ecde11f
beltervas <98856514+Terucsa@users.noreply.github.com>
2023-06-23 19:32:17 -0600

    Cambios al color del 0

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ function restarCuadro(){
     let restar = parseInt(document.getElementById("numeroContadorId").innerHTML) - 1;
     document.getElementById("numeroContadorId").innerHTML = restar;
     
-    if(restar < -0){
-        let colorActual = "red";
+    if(restar == 0){
+        let colorActual = "black";
         let styleElemento =document.getElementById("numeroContadorId");
         styleElemento.style.color = colorActual
-    }else if(restar == 0){
-        let colorActual = "black";
+    }else if(restar <= -1){
+        let colorActual = "red";
         let styleElemento =  document.getElementById("numeroContadorId");
         styleElemento.style.color = colorActual;
     }
@@ -18,12 +18,13 @@ function sumarCuadro(){
     let incrementar = parseInt(document.getElementById("numeroContadorId").innerHTML) + 1;;
     document.getElementById("numeroContadorId").innerHTML = incrementar;
 
-    if(incrementar > 0){
-        let colorActual = "green";
+    if(incrementar == 0){
+        let colorActual = "black";
         let styleElemento =  document.getElementById("numeroContadorId");
         styleElemento.style.color = colorActual;
-    }else if(incrementar == 0){
-        let colorActual = "black";
+    }else if(incrementar >= 1){
+        console.log(incrementar)
+        let colorActual = "green";
         let styleElemento =  document.getElementById("numeroContadorId");
         styleElemento.style.color = colorActual;
     }
@@ -32,5 +33,10 @@ function sumarCuadro(){
 function resetearCuadro(){
     let numeroCero = document.getElementById("numeroContadorId")
    numeroCero.innerHTML = 0;
+   if(numeroCero){
+        let colorActual = "black";
+        let styleElemento =  document.getElementById("numeroContadorId");
+        styleElemento.style.color = colorActual;
+   }
 }
 
